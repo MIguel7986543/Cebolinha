c31a2f17fd53b1e2f4c77b58869e35fb7d5e107c
Miguel <mdias1300@gmail.com>
2025-03-11 22:27:32 -0300

    atividade feita

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(@"Sou um galoto malandlinho e muito inteligente que semple esta tlamando 
+planos pala dellotar aquela golducha da Monica e me tolna o dono da lua!!");
+Console.ResetColor();
